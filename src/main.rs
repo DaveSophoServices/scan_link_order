baae6b2d2682dac304d9e653ec0126207e415bb3
Dave Smith <dave@sophoservices.com>
2021-06-25 16:29:28 -0500

    Add check for Owner in agent's name

--- src/main.rs
+++ src/main.rs
@@ -5,10 +5,15 @@ use std::fs;
 fn main() {
     let mut settings = config::Config::default();
     settings
-	.merge (config::File::with_name("link_scanner")).unwrap();
-    
-    let url = settings.get_str("url").unwrap();
+        .merge(config::File::with_name("link_scanner"))
+        .unwrap();
 
+    check_url_order(settings.get_str("url").unwrap());
+
+    check_owner_title(settings.get_str("owner_url").unwrap());
+}
+
+fn check_url_order(url:String) {
     let resp = reqwest::blocking::get(&url).unwrap();
     assert!(resp.status().is_success());
 
@@ -18,10 +23,10 @@ fn main() {
 
     let mut title_order: Vec<&str> = Vec::new();
     for property in frag.select(&properties) {
-	let property_title = property.text().collect::<Vec<_>>();
-	for s in property_title.iter() {
-	    title_order.push(s.trim());
-	}
+        let property_title = property.text().collect::<Vec<_>>();
+        for s in property_title.iter() {
+            title_order.push(s.trim());
+        }
     }
 
     let mut write_out = false;
@@ -29,37 +34,56 @@ fn main() {
     //println!("{:?}", title_order);
     // compare to previous run
     match fs::read("last-titles.dat") {
-	Ok(x) => {
-	    prev_list =bincode::deserialize(&x[..]).unwrap();
-	    if prev_list.iter().zip(&title_order)
-		.filter(|&(a,b)| a != b).count() > 0 {
-
-		    eprintln!("Order changed!\nCurrent:");
-		    for i in title_order.iter() {
-			eprintln!("{}",i);
-		    }
-		    eprintln!("Prev:");
-		    for i in prev_list {
-			eprintln!("{}",i);
-		    }
-		    write_out = true;
-		}
-	},
-	Err(e) => {
-	    match e.kind() {
-		std::io::ErrorKind::NotFound => {
-		    write_out = true;
-		},
-		_ => panic!("Failed to read strings from last-titles.dat: {:?}", e)
-	    }
-	}
+        Ok(x) => {
+            prev_list = bincode::deserialize(&x[..]).unwrap();
+            if prev_list
+                .iter()
+                .zip(&title_order)
+                .filter(|&(a, b)| a != b)
+                .count()
+                > 0
+            {
+                eprintln!("Order changed!\nCurrent:");
+                for i in title_order.iter() {
+                    eprintln!("{}", i);
+                }
+                eprintln!("Prev:");
+                for i in prev_list {
+                    eprintln!("{}", i);
+                }
+                write_out = true;
+            }
+        }
+        Err(e) => match e.kind() {
+            std::io::ErrorKind::NotFound => {
+                write_out = true;
+            }
+            _ => panic!("Failed to read strings from last-titles.dat: {:?}", e),
+        },
     };
 
     if write_out {
-	// serialize the title_order variable, which is the new
-	// sequence we just got
-	if let Err(e) = fs::write("last-titles.dat", bincode::serialize(&title_order).unwrap()) {
-	    panic!("Failed to write string to last-titles.dat: {:?}", e);
-	}
+        // serialize the title_order variable, which is the new
+        // sequence we just got
+        if let Err(e) = fs::write("last-titles.dat", bincode::serialize(&title_order).unwrap()) {
+            panic!("Failed to write string to last-titles.dat: {:?}", e);
+        }
     }
+}
+
+fn check_owner_title(url: String) {
+    let resp = reqwest::blocking::get(&url).unwrap();
+    assert!(resp.status().is_success());
+
+    let body = resp.text().unwrap();
+    let frag = Html::parse_document(&body);
+    let owner = Selector::parse(".agentName a").unwrap();
+
+    for owner_tag in frag.select(&owner) {
+        let o = owner_tag.value().attr("title").unwrap();
+        if o.contains("Owner") {
+            eprintln!("Word 'Owner' appears in agent's title: {:?}",o)
+        }        
+    }
+
 }
